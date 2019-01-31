720c789d87b463677f4ad5292bad5459128330cd
Kentarou-Orido <44537074+Kentarou-Orido@users.noreply.github.com>
2019-01-31 19:47:46 +0900

    Change root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'tests#index'
+  root 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
