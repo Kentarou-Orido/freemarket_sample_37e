d58ff2ce6289738519b14e688f956e08c557e71d
Miki Tatsuya <gorilladunk.tatsuya@gmail.com>
2019-01-31 23:50:59 +0900

    Change class name
    Class名をメルカリに揃えた
    Routeのresourceをまとめた
    profileのscssを一部を修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root 'tests#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :mypage do
-    resource :identifications, only: [:show]
-    resource :profile, only: [:show]
+    resource :profile,:identification, only: [:show]
   end
 end
