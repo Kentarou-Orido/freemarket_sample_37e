f5750732084f9b0f40817d76f5eed1f0b6286f6d
Kentarou-Orido <44537074+Kentarou-Orido@users.noreply.github.com>
2019-02-02 09:44:20 +0900

    Change root
    チーム規則に則りrootを変えた

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :mypage do
+  resource :users, only: [:show] do
     resource :profile, only: [:show]
   end
 end
