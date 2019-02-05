88552879c1357f18c670da547bd53b589d53bbd5
nozomi71 <skn.miu@gmail.com>
2019-02-05 21:28:01 +0900

    add cards controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :users, only: [:show] do
     resource :profile,:identification, only: [:show]
+    resource :card, only: [:index, :show]
   end
   resource :logout, only: [:show]
   resource :items, only: [:show, :index]
