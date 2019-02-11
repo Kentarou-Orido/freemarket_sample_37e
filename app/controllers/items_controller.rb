619a8cc6c14788e0d249b99b550a830a1b678e08
nozomi71 <skn.miu@gmail.com>
2019-02-11 15:43:47 +0900

    masterを変更するため一旦commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,16 @@
 class ItemsController < ApplicationController
 
   def index
+    # binding.pry
+    # item = Item.find(params[:id])
+    # @category = item.categories
   end
 
   def show
   end
+
+  private
+    def item_params
+      params.require(:item).permit(:name)
+    end
 end
