134df0930344840793f5b02b403f472b1a0abc9a
IshinoJun <junishino@junnoMacBook-puro.local>
2019-02-16 16:38:44 +0900

    クレジット決済処理を追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,25 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
+
   end
 
-  def buy
-  end
+  def purchase
+    @item = Item.find(params[:format])
+   end
 
+  def completed_purchase
+    @item = Item.find(params[:id])
+    require 'payjp'
+    Payjp.api_key = PAYJP_SECRET_KEY
+
+    Payjp::Charge.create(
+      amount:  @item.price,
+      card:    params['payjp-token'],
+      currency: 'jpy',
+    )
+    @item.update!(buyer_id: '1')
+    binding.pry
+  end
 end
