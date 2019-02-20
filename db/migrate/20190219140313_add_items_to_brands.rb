c24354cc4873a9d100441da5878e8f52a2efe42f
Kentarou-Orido <44537074+Kentarou-Orido@users.noreply.github.com>
2019-02-20 18:44:00 +0900

    Change migrate file

--- db/migrate/20190219140313_add_items_to_brands.rb
+++
@@ -1,6 +0,0 @@
-class AddItemsToBrands < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :items, :brand, null:false, foreign_key: true
-    add_reference :items, :category, null:false, foreign_key: true
-  end
-end
