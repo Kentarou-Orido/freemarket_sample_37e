2207ae6aa2e39d3390153cbc7a8dcf4629b93b80
Kentarou-Orido <44537074+Kentarou-Orido@users.noreply.github.com>
2019-02-22 00:01:35 +0900

    rechange seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
-load(Rails.root.join( 'db', 'seeds', "#{Rails.env.downcase}.rb"))
-
+Item.create(id: "1", name: "激安！ルイビトンのカバン❤", price: "100", user_id: "1", trade_status: "1", item_condition: "1", postage: "100", area: "東京都", shipping_method: "1", size: "m", text: "やすいよ〜", created_at: "2019-02-21 23:00:00", updated_at: "2019-02-21 23:00:00", seller_id: "1", buyer_id: NULL)
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
