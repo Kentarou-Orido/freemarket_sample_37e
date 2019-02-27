fafae9775acd82cbfe1f1ff8f5a4dbe8a632350c
Kentarou-Orido <44537074+Kentarou-Orido@users.noreply.github.com>
2019-02-27 23:35:19 +0900

    Change header for Login user or logout user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def confirming_login_for_head_bar
+
+    if user_signed_in?
+      render "default/default-header-login"
+    else
+      render "default/default-header-not-login"
+    end
+  end
 end
