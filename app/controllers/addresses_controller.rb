f43ce039688c50d761010bfbeb19b13a4bb10e43
Kentarou-Orido <44537074+Kentarou-Orido@users.noreply.github.com>
2019-02-27 23:34:40 +0900

    Add signup section

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,19 +1,26 @@
 class AddressesController < ApplicationController
+
   def new
-    @address = Address.new
+    creating_address_instance
     @user = User.find(params[:user_id])
   end
 
   def create
-    if @group.save
-      redirect_to root_path, notice: 'グループを作成しました'
+    creating_address_instance(addresse_params)
+    if @address.save
+      redirect_to root_path, notice: "新規登録が完了しました！ようこそ！"
     else
-      render :new
+      render :new, notice: "新規登録に失敗しました。もう一度、入力をお願い致します"
     end
   end
 
-    private
+  private
+
+  def creating_address_instance
+    @address = Address.new
+  end
+
   def addresse_params
-    params.require(:addresses).permit(:postcode, :prefectures, :municipality, :street_number, :building_name, :room_number, :remarks, { :user_ids => [] })
+    params.require(:address).permit(:postcode, :prefectures, :municipality, :street_number, :building_name, :room_number, :remarks).merge(user_id: params[:user_id])
   end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
   prepend_before_action :check_captcha, only: [:create]
 
-  private
+
     def check_captcha
-      unless verify_recaptcha secret_key: "6LcqppIUAAAAAPk8H5lvq90DIxnU5xXJoq6lyfUy"
+      unless verify_recaptcha
         self.resource = resource_class.new sign_up_params
         resource.validate
         set_minimum_password_length
@@ -11,11 +11,12 @@ class RegistrationsController < Devise::RegistrationsController
       end
     end
 
+    def get_user_id(resource)
+      @user = User.find(params[:id])
+    end
+
     def after_sign_up_path_for(resource)
       new_user_address_path(@user)
     end
 
-    def get_user_id
-      @user = User.find(params[:id])
-    end
 end
