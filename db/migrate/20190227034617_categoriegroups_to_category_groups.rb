9ca4bac5814a8707182936251cd8e8ae1df4a765
Kentarou-Orido <44537074+Kentarou-Orido@users.noreply.github.com>
2019-02-28 17:41:56 +0900

    Change migrate sentence

--- db/migrate/20190227034617_categoriegroups_to_category_groups.rb
+++ db/migrate/20190227034617_categoriegroups_to_category_groups.rb
@@ -1,5 +1,5 @@
 class CategoriegroupsToCategoryGroups < ActiveRecord::Migration[5.0]
   def change
-    rename_table :Categoriegroups, :categories_groups
+    rename_table :categoriegroups, :categories_groups
   end
 end
