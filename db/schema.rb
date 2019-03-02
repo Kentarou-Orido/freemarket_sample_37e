1448396207c0740a5015cc546e5eab14610f70bc
IshinoJun <t.mac.jun.0303@gmail.com>
2019-03-02 16:19:19 +0900

    Itemsテーブルのareaの型を変更
    String⇨integerに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190302063404) do
+ActiveRecord::Schema.define(version: 20190302071253) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "postcode",                    null: false
@@ -42,11 +42,9 @@ ActiveRecord::Schema.define(version: 20190302063404) do
   end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string  "name",     null: false
-    t.string  "ancestry"
-    t.integer "item_id",  null: false
+    t.string "name",     null: false
+    t.string "ancestry"
     t.index ["ancestry"], name: "index_categories_on_ancestry", using: :btree
-    t.index ["item_id"], name: "index_categories_on_item_id", using: :btree
   end
 
   create_table "categories_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -80,7 +78,7 @@ ActiveRecord::Schema.define(version: 20190302063404) do
     t.string   "name",                          null: false
     t.integer  "price",                         null: false
     t.integer  "item_condition",                null: false
-    t.string   "area",                          null: false
+    t.integer  "area",                          null: false
     t.integer  "shipping_method",               null: false
     t.string   "size"
     t.text     "text",            limit: 65535, null: false
@@ -94,13 +92,6 @@ ActiveRecord::Schema.define(version: 20190302063404) do
     t.index ["brand_id"], name: "index_items_on_brand_id", using: :btree
     t.index ["buyer_id"], name: "index_items_on_buyer_id", using: :btree
     t.index ["seller_id"], name: "index_items_on_seller_id", using: :btree
-  end
-
-  create_table "tests", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_tests_on_name", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
