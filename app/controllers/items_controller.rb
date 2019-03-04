093ae89d06704f530f200407bb1547a30ec15256
IshinoJun <t.mac.jun.0303@gmail.com>
2019-03-04 22:11:10 +0900

    ItemのコントローラーをDBから取得

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,19 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show,:completed_purchase]
+  before_action :set_item, only: [:show,:purchase,:completed_purchase]
+  before_action :set_image, only: [:show,:purchase]
 
   def index
   end
 
   def show
+    @seller = @item.seller
+    @brand = Brand.find(@item.brand_id)
+    @categories = @item.categories
+    @saling_items = Item.where(seller_id: @item.seller_id).where.not(id: @item.id)
+    @other_items = @categories[-1].items.where(brand_id: @item.brand_id).where.not(id: @item.id)
   end
 
   def purchase
-    @item = Item.find(params[:format])
   end
 
   def completed_purchase
@@ -20,6 +25,7 @@ class ItemsController < ApplicationController
         card:    params['payjp-token'],
         currency: 'jpy',
       )
+      # 本当はcurrent_user.id
       @item.update!(buyer_id: 1)
     end
   end
@@ -29,4 +35,9 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+
+  def set_image
+    @images = @item.images
+  end
 end
+
