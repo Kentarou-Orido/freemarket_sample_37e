dca486baa30ae9d64dc85a1e07770899e65e7de0
Kentarou-Orido <44537074+Kentarou-Orido@users.noreply.github.com>
2019-03-06 02:58:29 +0900

    Delete creating_address_instance method

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,25 +1,21 @@
 class AddressesController < ApplicationController
 
   def new
-    creating_address_instance
+    @address = Address.new
     @user = User.find(params[:user_id])
   end
 
   def create
-    creating_address_instance(addresse_params)
+    @address = Address.new(addresse_params)
     if @address.save
       redirect_to root_path, notice: "新規登録が完了しました！ようこそ！"
     else
-      render :new, notice: "新規登録に失敗しました。もう一度、入力をお願い致します"
+      @user = User.find(params[:user_id])
+      render :new
     end
   end
 
   private
-
-  def creating_address_instance
-    @address = Address.new
-  end
-
   def addresse_params
     params.require(:address).permit(:postcode, :prefectures, :municipality, :street_number, :building_name, :room_number, :remarks).merge(user_id: params[:user_id])
   end
