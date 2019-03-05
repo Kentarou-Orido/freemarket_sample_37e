ec67f2d5706cd81665c023fe7aae9249bae28f6e
Kentarou-Orido <44537074+Kentarou-Orido@users.noreply.github.com>
2019-03-06 03:02:05 +0900

    Change helper method sentence

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,2 @@
 module UsersHelper
-  def confirming_login_for_head_bar
-
-    if user_signed_in?
-      render "default/default-header-login"
-    else
-      render "default/default-header-not-login"
-    end
-  end
 end
