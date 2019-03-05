83daa57dbd77ec1fe1086f2b51233e7fa20bba5f
Kentarou-Orido <44537074+Kentarou-Orido@users.noreply.github.com>
2019-03-06 03:11:08 +0900

    Set spec files

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -3,15 +3,14 @@ FactoryGirl.define do
     id              "1"
     name            "商品"
     price           "1000"
-    user_id         "1"
-    trade_status    "1"
     item_condition  "1"
-    postage         "1"
     area            "1"
     shipping_method "1"
     size            "1"
     text            "1"
     seller_id       "1"
     buyer_id        "0"
+    delivery_date   "1"
+    delivery_burden "1"
   end
 end
