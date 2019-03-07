1c54b1fb503448046139cc70ae611e3af0219dae
Miki Tatsuya <gorilladunk.tatsuya@gmail.com>
2019-03-07 21:55:57 +0900

    Add instance
    変数の定義
    paginateの導入

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,8 @@
 class ListingsController < ApplicationController
 
   def show
+    user = User.find(params[:id])
+    @items = user.items.page(params[:page]).per(5).order("created_at DESC")
   end
 
 end
