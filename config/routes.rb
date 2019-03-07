16ef4b442816237b4f6eef3bb0f6784e310ed9b7
Kentarou-Orido <44537074+Kentarou-Orido@users.noreply.github.com>
2019-03-07 13:42:14 +0900

    Delete extra sentences
    消し忘れていた記述を削除した

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   root 'items#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :new] do
+  resources :users, only: [:show] do
     resources :profile,:identification, only: [:show]
     resources :cards, only: [:index, :new]
     resources :addresses, only: [:new, :create]
