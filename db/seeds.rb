a2f101fc01368059e84e9197bb172e84b6749672
IshinoJun <t.mac.jun.0303@gmail.com>
2019-03-09 16:55:08 +0900

    Seedsを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,36 @@ end
   birthday = Faker::Date.birthday(18, 65)
   User.create!(email: email, password: password, password_confirmation: password,nickname: nickname, family_name: family_name, first_name: first_name, family_name_kana: family_name_kana, first_name_kana: first_name_kana, telephone: telephone, birthday: birthday)
 end
+
+50.times do
+  Item.create!(
+    name: Faker::Dessert.variety,
+    price: Faker::Number.number(6),
+    item_condition: Faker::Number.between(0, 2),
+    area: Faker::Number.between(0, 46),
+    shipping_method: Faker::Number.between(0, 8),
+    text: "sample",
+    size: Faker::Number.between(0, 9),
+    created_at: Faker::Time.between(DateTime.now - 1, DateTime.now),
+    updated_at: Faker::Time.between(DateTime.now - 1, DateTime.now),
+    delivery_date: Faker::Number.between(0, 2),
+    delivery_burden: Faker::Number.between(0, 1),
+    brand_id: Faker::Number.between(0, 3)
+  )
+
+  CategoriesGroup.create!(
+    item_id: Faker::Number.between(0, 49),
+    category_id: Faker::Number.between(0, 3)
+  )
+
+end
+
+Category.create!(name: "レディース", item_id: 1)
+Category.create!(name: "メンズ", item_id: 2)
+Category.create!(name: "ベビー・キッズ", item_id: 3)
+Category.create!(name: "コスメ・香水・美容", item_id: 4)
+
+Brand.create!(name: "シャネル", item_id: 1)
+Brand.create!(name: "ルイヴィトン", item_id: 2)
+Brand.create!(name: "シュプリーム", item_id: 3)
+Brand.create!(name: "ナイキ", item_id: 4)
