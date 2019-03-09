a1232feaea0cea7d6957c565bc6ead6835cce00d
Kentarou-Orido <44537074+Kentarou-Orido@users.noreply.github.com>
2019-03-10 01:25:43 +0900

    Change directory

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 require "csv"
 
-CSV.foreach('db/category.csv', encoding: 'Shift_JIS:UTF-8') do |row|
+CSV.foreach('current/app/db/category.csv', encoding: 'Shift_JIS:UTF-8') do |row|
   Category.create(:name => row[0], :ancestry => row[1])
 end
 
