7a2a26ba0ad5375f8d99f2c07bc9c45905df945e
Miki Tatsuya <gorilladunk.tatsuya@gmail.com>
2019-03-10 17:17:54 +0900

    Edit kaminari
    カミナリgemのビュー追加

--- app/controllers/listing_controller.rb
+++ app/controllers/listing_controller.rb
@@ -1,14 +1,9 @@
 class ListingController < ApplicationController
 
-  before_action :set_user
-
   def index
-    @items = @user.items.page(params[:page]).per(5).order("created_at DESC")
+    @items =  Item.where(seller_id: 1).page(params[:page]).per(5).order("created_at DESC")
   end
 
   private
-  def set_user
-    @user = current_user
-  end
 
 end
