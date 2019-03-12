644d6ddda0a1c209040bcb2a5a58403683e4ebe1
Kentarou-Orido <44537074+Kentarou-Orido@users.noreply.github.com>
2019-03-13 01:39:15 +0900

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -52,8 +52,8 @@ ActiveRecord::Schema.define(version: 20190312135717) do
     t.integer  "category_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["category_id"], name: "index_categories_groups_on_category_id", using: :btree
-    t.index ["item_id"], name: "index_categories_groups_on_item_id", using: :btree
+    t.index ["category_id"], name: "index_categoriegroups_on_category_id", using: :btree
+    t.index ["item_id"], name: "index_categoriegroups_on_item_id", using: :btree
   end
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
