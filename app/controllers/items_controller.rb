7bcfabb42ac0c9dc9db03bfec4395978d25c5e9a
nozomi71 <skn.miu@gmail.com>
2019-03-15 21:07:17 +0900

    add if

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,7 +30,7 @@ class ItemsController < ApplicationController
 
   def destroy
     @item = Item.find(params[:id])
-    @item.destroy if item.seller_id == current_user.id
+    @item.destroy if @item.seller_id == current_user.id
   end
 
   def purchase
